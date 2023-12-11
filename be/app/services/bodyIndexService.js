ab8b989a539a8f6bdf222f8109bcb67b6ae8ee95
Huy0110 <duchuy01102001@gmail.com>
2023-12-11 14:17:57 +0000

    Fix the bodyFat calculation formula

--- be/app/services/bodyIndexService.js
+++ be/app/services/bodyIndexService.js
@@ -3,9 +3,9 @@
 class BodyIndexService {
   static calculateBodyFatIndex(gender, neck, waist, hip, height) {
     if (gender.toLowerCase() === 'male') {
-      return 495 / (1.0324 - 0.19077 * Math.log(waist - neck) + 0.15456 * Math.log(height)) - 450
+      return 495 / (1.0324 - 0.19077 * Math.log10(waist - neck) + 0.15456 * Math.log10(height)) - 450
     } else if (gender.toLowerCase() === 'female') {
-      return 495 / (1.29579 - 0.35004 * Math.log(waist + hip - neck) + 0.221 * Math.log(height)) - 450
+      return 495 / (1.29579 - 0.35004 * Math.log10(waist + hip - neck) + 0.221 * Math.log10(height)) - 450
     } else {
       throw new Error('Invalid gender. Use "male" or "female".')
     }
