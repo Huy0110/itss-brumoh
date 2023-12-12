bfd48b2f7c6a1dd2ef92907be151b1d7123a26ef
Nguyen Duong <muoi07052001@gmail.com>
2023-12-12 08:54:25 +0700

    fix: format

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import MainLayout from './components/layout/MainLayout'
-import AppLayout from './components/layout/AppLayout'
 import LoginPage from './pages/LoginPage'
 import Auth from './hooks/auth'
 import BodyFat from './pages/BodyFat'
@@ -12,9 +11,6 @@ const App = () => {
   return (
     <div>
       <Routes>
-        {/* <Route element={<AppLayout />}>
-          <Route index element={<LoginPage />} path="/login" />
-        </Route> */}
         <Route element={<MainLayout />}>
           <Route element={<BodyParam />} path="/bodyparam" />
           <Route element={<LoginPage />} path="/login" />
