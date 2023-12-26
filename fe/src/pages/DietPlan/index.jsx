ea0d7cdac1373ff748e898f7a8a679f25703e789
Minh Hải Vũ <minhhaivu@Minhs-MacBook-Pro-2.local>
2023-12-26 15:48:29 +0700

    Remove unused codes and comments

--- fe/src/pages/DietPlan/index.jsx
+++ fe/src/pages/DietPlan/index.jsx
@@ -1,22 +1,11 @@
-// import React, { useEffect, useState } from 'react'
 import Header from '../../components/Header'
 import './style.css'
 import { useNavigate } from 'react-router-dom'
-// import { useLocation } from 'react-router-dom'
-// import USER from '../../services/userService'
 
 export default function DietPlan() {
-  // const location = useLocation()
-  // const currentPath = location.pathname
-  // const id = currentPath.split('/')[2]
-  let breakfast = localStorage.getItem('breakfast')
-  let lunch = localStorage.getItem('lunch')
-  let dinner = localStorage.getItem('dinner')
-  let allday = localStorage.getItem('allday')
+
   const navigate = useNavigate()
-  // const [video, setVideo] = useState('')
-  // const [description, setDescription] = useState('')
-  // const [effect, setEffect] = useState('')
+
   const handleClick = () => {
     navigate('/meal-guide')
   }
