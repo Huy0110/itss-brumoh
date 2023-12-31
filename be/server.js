6ecf3051503658d811010bb35a2709ee8f49f8c9
Huy0110 <duchuy01102001@gmail.com>
2023-12-31 05:04:47 +0000

    Update http to https

--- be/server.js
+++ be/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const db = require('./app/db')
+const https = require('https')
+const fs = require('fs')
+const path = require('path')
 require('dotenv').config()
 
 const app = express()
@@ -29,6 +32,16 @@ require('./app/routes/training.routes')(app)
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8088
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`)
+
+// Read SSL certificate and key files
+const options = {
+  key: fs.readFileSync(path.join(__dirname, 'localhost-key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'localhost.pem'))
+}
+
+// Create HTTPS server
+const server = https.createServer(options, app)
+
+server.listen(PORT, () => {
+  console.log(`App listening on port: ${PORT}`)
 })
